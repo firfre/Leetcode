d0c7da5c114626eb70b9ff4f9e838e05a47979c6
gzhang71 <arthur.zhanggq@gmail.com>
2018-12-20 15:55:18 -0500

    Update TwoSum.py

--- python/venv/Scripts/TwoSum.py
+++ python/venv/Scripts/TwoSum.py
@@ -9,8 +9,7 @@ class Solution(object):
             else:
                 a[num] = i
 
-def main():
-    aSolution = Solution()
-    print(aSolution.twoSum([2, 5, 7, 11], 12))
 
-main()
+aSolution = Solution()
+print(aSolution.twoSum([2, 5, 7, 11], 12))
+
