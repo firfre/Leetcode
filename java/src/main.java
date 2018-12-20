3a7bc49e1f03938f00f7ed6100cb2ea6724176fd
gzhang71 <arthur.zhanggq@gmail.com>
2018-12-20 14:27:04 -0500

    Update main.java

--- java/src/main.java
+++ java/src/main.java
@@ -2,6 +2,7 @@ public class main {
 
     public static void testTwoSum () {
         Solution aSolution = new Solution();
+        int[] number = {2, 7, 11, 15};
         int[] result = aSolution.twoSum(number, 22);
 
         System.out.printf("Found index is %d and %d", result[0], result[1]);
