2a32c7a9ca8a47880bc64156308486e34ca45b20
gzhang71 <arthur.zhanggq@gmail.com>
2018-12-23 18:48:29 -0500

    update subsetII

---
+++ java/src/SubsetII.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
+
+class SubsetII {
+    static List<List<Integer>> solution(int[] numbers) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        result.add(new ArrayList<Integer>());
+        Arrays.sort(numbers);
+        int begin = 0;
+
+        for(int i = 0; i < numbers.length; i++) {
+
+            if(i == 0 || numbers[i] == numbers[i - 1]) {
+                begin = 0;
+            } else {
+                begin = resultSize;
+            }
+            int resultSize = result.size();
+            for (int j = begin; j < resultSize ; j++) {
+                List<Integer> aSubset = new ArrayList<Integer>(result.get(j));
+                aSubset.add(numbers[i]);
+                result.add(aSubset);
+            }
+        }
+
+        return(result);
+    }
+
+    public static void test() {
+        int[] numbers = {1, 2, 3};
+        List<List<Integer>> result = solution(numbers);
+        for(List<Integer> oneSubSet : result) {
+            System.out.println(oneSubSet);
+        }
+    }
+}
